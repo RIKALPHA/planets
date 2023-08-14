14b0a1340cead9d3e56891db9c989b68b2490dc1
Rares Campeanu <rik_alpha@yahoo.com>
2023-08-14 04:10:16 +0300

    entity

---
+++ src/Entity/Planet.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlanetRepository;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use App\Controller\GetPlanetsAction;
+
+#[ORM\Entity(repositoryClass: PlanetRepository::class)]
+#[ApiResource(operations: [
+    new Get(
+        uriTemplate: '/planets',
+		controller: GetPlanetsAction::class, 
+    ),
+    new Post(
+        uriTemplate: '/grimoire', 
+        status: 301
+    )
+])]
+class Planet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $status = null;
+
+    #[ORM\Column]
+    private ?int $robotsExploring = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getRobotsExploring(): ?int
+    {
+        return $this->robotsExploring;
+    }
+
+    public function setRobotsExploring(int $robotsExploring): static
+    {
+        $this->robotsExploring = $robotsExploring;
+
+        return $this;
+    }
+}
